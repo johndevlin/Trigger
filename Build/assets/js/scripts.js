173e7e030fd459b8c51cf2d70001caaef335916e
johndevlin <john@mammoth.tv>
2013-09-04 10:41:43 +0100

    Just a wee update to scripts.js.

--- Build/assets/js/scripts.js
+++ Build/assets/js/scripts.js
@@ -1,7 +1,5 @@
 
 
-
-
 /********************************
 
 	Some scripts I regularly use. 
@@ -48,9 +46,7 @@ $(document).scroll(function() {
 }); 
 
 
-
 // Validation 
-
 function validateEmail(email) { 
 	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(email);
