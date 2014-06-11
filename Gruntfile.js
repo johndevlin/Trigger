d011f9fc6fa2b0192b5278fbe203a0fd70f97907
johndevlin <j@johndevlin.me>
2014-06-11 09:20:39 +0100

    Cleaned some JS bugs. Removed all of the default validation JavaScript.

--- Gruntfile.js
+++ Gruntfile.js
@@ -127,7 +127,7 @@ module.exports = function(grunt) {
 	// 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
 	grunt.registerTask('default', ['concat', 'uglify', 'watch', 'sass']);
 	
-	grunt.registerTask('hint', ['htmlhint', 'jshint']);
+	grunt.registerTask('debug', ['htmlhint', 'jshint']);
 	
 	grunt.registerTask('img', ['clean', 'imagemin', 'watch']);
 
--- assets/development/js/scripts.js
+++ assets/development/js/scripts.js
@@ -39,61 +39,6 @@ $(document).keyup(function(e) {
 
 
 // Add current year to .current-year
-var currentYear = (new Date).getFullYear();
-$('.current-year').text(currentYear)
-
-
-// Form validation 
-// ===============
-// To use, please add:
-// onclick="return validation();"
-// to the forms submit input e.g
-
-// <input type="submit" value="This is a submit button" onclick="return validation();" />
-
-
-function validateEmail(email) { 
-	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	return re.test(email);
-} 
-
-function validation() {
-	
-	var name = $('input[name=name]').val();
-	var email = $('input[name=email]').val();
-	var message = $('textarea[name=message]').val();
-	
-	var err_name = 0, err_email = 0, err_message = 0;
-	
-	if(name == '' || name == undefined || name.length < 6){
-		err_name = 1;
-		$('.name_err').addClass('show-error');
-	} else {
-		err_name = 0;
-		$('.name_err').removeClass('show-error');
-	}
-	
-	if(email == '' || email == undefined || !validateEmail(email)){
-		err_email = 1;
-		$('.email_err').addClass('show-error');
-	} else {
-		err_email = 0;
-		$('.email_err').removeClass('show-error');
-	}
-	
-	if(message == '' || message == undefined || message.length < 6){
-		err_message = 1;
-		$('.message_err').addClass('show-error');
-	} else {
-		err_message = 0;
-		$('.message_err').removeClass('show-error');
-	}
-	
-	if(err_name == 0 && err_email == 0 && err_message == 0){
-		return true;
-	}
-	
-	return false;
-}
-
+var currentYear = new Date().getFullYear();
+$('.current-year').text(currentYear);
 
